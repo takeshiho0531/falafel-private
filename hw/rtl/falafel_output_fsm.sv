626f434d05e8acaca0621ec0cc5dec7a8605ed3a
Joan Farres <joan.farres.garcia@estudiantat.upc.edu>
2024-04-08 17:15:22 -0700

    Add duplicated output for cohort

--- hw/rtl/falafel_output_fsm.sv
+++ hw/rtl/falafel_output_fsm.sv
@@ -1,10 +1,8 @@
 module falafel_output_fsm
   import falafel_pkg::*;
 (
-    // verilator lint_off UNUSEDSIGNAL
     input logic clk_i,
     input logic rst_ni,
-    // verilator lint_on UNUSEDSIGNAL
 
     //-------------- response ---------------//
     input  logic              rsp_rdy_i,
@@ -17,15 +15,51 @@ module falafel_output_fsm
     input  logic [DATA_W-1:0] resp_fifo_dout_i
 );
 
+  logic [DATA_W-1:0] buffer_d, buffer_q;
+  logic flag_d, flag_q; // 1 if we have to send the copy
+
+  assign buffer_d = resp_fifo_read_o ? resp_fifo_dout_i : buffer_q;
+
   always_comb begin
     resp_fifo_read_o = 1'b0;
     rsp_val_o = 1'b0;
     rsp_data_o = resp_fifo_dout_i;
 
-    if (rsp_rdy_i && !resp_fifo_empty_i) begin
-      resp_fifo_read_o = 1'b1;
-      rsp_val_o = 1'b1;
-      rsp_data_o = resp_fifo_dout_i;
+    flag_d = flag_q;
+
+    if (rsp_rdy_i && (!resp_fifo_empty_i || flag_q)) begin
+      if (flag_q) begin
+        flag_d = 1'b0;
+        rsp_val_o = 1'b1;
+        rsp_data_o = buffer_q;
+      end else begin
+        flag_d = 1'b1;
+        resp_fifo_read_o = 1'b1;
+        rsp_val_o = 1'b1;
+        rsp_data_o = resp_fifo_dout_i;
+      end
     end
   end
+
+  always_ff @(posedge clk_i) begin
+    if (!rst_ni) begin
+      buffer_q <= '0;
+      flag_q <= 1'b0;
+    end else begin
+      buffer_q <= buffer_d;
+      flag_q <= flag_d;
+    end
+  end
+
+  // always_comb begin
+  //   resp_fifo_read_o = 1'b0;
+  //   rsp_val_o = 1'b0;
+  //   rsp_data_o = resp_fifo_dout_i;
+
+  //   if (rsp_rdy_i && !resp_fifo_empty_i) begin
+  //     resp_fifo_read_o = 1'b1;
+  //     rsp_val_o = 1'b1;
+  //     rsp_data_o = resp_fifo_dout_i;
+  //   end
+  // end
 endmodule
