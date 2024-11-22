3aef4d7e6afc4f42b11eba15e302540dcabfe893
takeshiho0531 <kawadatakeshiho@gmail.com>
2024-11-22 15:40:58 -0800

    fix: fic case > MIN_ALLOC_SIZE

--- dev/falafel_core.sv
+++ dev/falafel_core.sv
@@ -103,13 +103,14 @@ module falafel_core
           state_d = REQ_LOAD_HEADER;
         end else begin
           header_data_prev_d.addr = prev_header_data_q.addr;
-          header_data_prev_d.next_addr = header_data_from_lsu_q.addr + 64 + size_to_allocate_q; // TODO
           if (header_data_from_lsu_q.size - size_to_allocate_q >= MIN_ALLOC_SIZE) begin
             state_d = REQ_INSERT_NEW_HEADER;
             header_data_to_insert_d.addr = header_data_from_lsu_q.addr + 64 + size_to_allocate_q;  // TODO
             header_data_to_insert_d.size = header_data_from_lsu_q.size - size_to_allocate_q;
             header_data_to_insert_d.next_addr = header_data_from_lsu_q.next_addr;
+            header_data_prev_d.next_addr = header_data_to_insert_d.addr;
           end else begin
+            header_data_prev_d.next_addr = header_data_from_lsu_q.next_addr;
             state_d = REQ_DELETE_HEADER;
           end
         end
